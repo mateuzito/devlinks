2948d902c34ba0163838cb58b1c051ebd550f291
mateuzito <mateuszitosilva@gmail.com>
2025-01-08 17:11:56 -0300

    Some changes to the list of links and improvements to user security

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 function toggleMode() {
-    const html = document.documentElement
-    html.classList.toggle('light')
+  const html = document.documentElement;
+  html.classList.toggle("light");
 
-    const img = document.querySelector("#profile img")
+  const img = document.querySelector("#profile img");
 
-    if(html.classList.contains('light')) {
-        img.setAttribute('src', './assets/avatar-light.png')
-        img.setAttribute('alt', 'Foto de Mateus Silva sério, usando óculos e camisa cinza, bigode e fundo rosa claro.')
-
-    } else {
-        img.setAttribute('src', './assets/avatar.png')
-        img.setAttribute('alt', 'Foto de Mateus Silva sério, usando óculos e camisa cinza, bigode e fundo branco.')
-    }
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png");
+    img.setAttribute(
+      "alt",
+      "Foto de Mateus Silva sério, usando óculos e camisa cinza, bigode e fundo rosa claro."
+    );
+  } else {
+    img.setAttribute("src", "./assets/avatar.png");
+    img.setAttribute(
+      "alt",
+      "Foto de Mateus Silva sério, usando óculos e camisa cinza, bigode e fundo branco."
+    );
+  }
 }
